1a6fbb02f64aa2ba00409f363f8cb756e53a0aab
guillermoRF2001 <guillermo.ramos.fer@gmail.com>
2025-02-03 12:50:20 +0100

    chat V0.1

---
+++ chat-react/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import {io} from 'socket.io-client';
+import {useEffect, useState} from'react';
+import { UlMensajes, LiMensaje } from './ui-components';
+
+const socket = io('http://localhost:3000');
+
+function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [nuevoMensaje,setNuevoMensaje] = useState('');
+  const [mensajes,setMensajes] = useState([]);
+
+  useEffect(() => {
+    socket.on('connect', () => setIsConnected(true));
+
+    socket.on('chat_message', (data) => {
+      setMensajes(mensajes => [...mensajes, data]);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('chat_message');
+    }
+  }, []);
+
+  const enviarMensaje = () => { 
+    socket.emit('chat_message',{
+      //Se podria enviar id de usuario enmede de socket id ya que este cambia con cada recarga de la pagina.
+      usuario: socket.id,
+      texto: nuevoMensaje,
+    });  
+  }
+
+  return (
+    <div className="App">
+      <h2>{isConnected ? 'Conectado': 'No conectado'}</h2>
+      <UlMensajes>
+        {mensajes.map(mensaje => (
+          <LiMensaje>{mensaje.usuario}:{mensaje.texto}</LiMensaje>
+        ))}
+        
+      </UlMensajes>
+      <input type="text"  onChange={e=>setNuevoMensaje(e.target.value)}/>
+      <button onClick={enviarMensaje}> Enviar</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer();
+
+const io = require('socket.io')(server,{
+    cors: {origin: '*'}
+});
+
+io.on('connection', (socket) => {
+    console.log('connection established');
+
+    socket.broadcast.emit('chat_message', {
+        usuario:'INFO',
+        texto: 'Nuevo usuario conectado'
+    });
+
+    socket.on('chat_message', (data) =>{
+        io.emit('chat_message', data);
+        
+    })
+
+});
+
+server.listen(3000);
